03402f7d51533b618bc754cff9ab720b89584ff6
Yujin Kim <45589219+yujuck@users.noreply.github.com>
2024-09-24 16:41:51 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction ci/cd1 test
+          Learn GithubAction ci/cd1 test2
         </a>
       </header>
     </div>
